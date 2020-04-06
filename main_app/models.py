8ba116e184eb3836016ea7230bea4043e5f6faa3
Waqas D <wdaimee@gmail.com>
2020-04-06 18:37:00 -0400

    added car models to models.py and migrated changes

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    color = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
