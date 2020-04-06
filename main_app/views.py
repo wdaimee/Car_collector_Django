37e5d44edbdfb3317ebdede61d85ee5143a9ba83
Waqas D <wdaimee@gmail.com>
2020-04-06 14:27:38 -0400

    corrected contedn in about HTML

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
     return HttpResponse('<h1>Hello Cars!</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About the Car Collector</h1>')
+    return render(request, 'about.html')
