9165cac6753df2193efcf5766f304b5e269df872
Waqas D <wdaimee@gmail.com>
2020-04-06 14:25:17 -0400

    added about urls and about.html boiler plate

--- carcollector/urls.py
+++ carcollector/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urs')),
+    path('', include('main_app.urls')),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Hello Cars!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About the Car Collector</h1>')
