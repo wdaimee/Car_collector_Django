0fca4d503114e0016caa6488a1a470f0de6f216a
Waqas D <wdaimee@gmail.com>
2020-04-07 22:21:29 -0400

    added Maintenace model and made migrations

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,12 @@ from django.urls import reverse
 
 # Create your models here.
 
+MAINT_TYPES = (
+    ('O', 'Oil Change'),
+    ('T', 'Tire Rotation'),
+    ('B', 'Brake Service')
+)
+
 class Car(models.Model):
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
@@ -15,3 +21,16 @@ class Car(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'car_id': self.id})
+
+class Maintenance(models.Model):
+    date = models.DateField()
+    maint_type = models.CharField(
+        max_length=1,
+        choices=MAINT_TYPES,
+        default=MAINT_TYPES[0][0]
+        )
+
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_maint_type_display()} on {self.date}"
