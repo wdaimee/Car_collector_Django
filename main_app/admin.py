a8181d7d244dd435f4d02dcc225f76c88474b778
Waqas D <wdaimee@gmail.com>
2020-04-08 20:36:16 -0400

    added Photo models to admin.py file, added markup to details.html to display add image form

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Car, Maintenance
+from .models import Car, Maintenance, Photo
 # Register your models here.
 admin.site.register(Car)
 admin.site.register(Maintenance)
+admin.site.regiester(Photo)
